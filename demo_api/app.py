620b57d0abd0992c39729099804e7fb1645f95be
AdrianPb <adrian@playbrush.com>
2022-01-13 17:36:28 +0100

    Include 404 for /prices

--- demo_api/app.py
+++ demo_api/app.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 
 from demo_api.price import amount_to_price
 
@@ -15,5 +15,8 @@ def root_get():
 def crypto_to_currency(crypto: str, currency: str, crypto_amount: Decimal):
     crypto_options = ['btc', 'eth', 'xrp']
     currency_options = ['usd', 'eur', 'gbp', 'bch', 'xlm']  # Only some crypto examples
+    if crypto not in crypto_options or currency not in currency_options:
+        return status.HTTP_404_NOT_FOUND
+
     return amount_to_price(crypto, currency, crypto_amount)
 
